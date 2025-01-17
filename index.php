aadb798940b9e3e4a71ee50ed01c27ff96580d09
DA-ROS Alain <147141828+DAAlain@users.noreply.github.com>
2025-01-17 14:47:51 +0100

    changement bouton se connecter

--- index.php
+++ index.php
@@ -8,14 +8,20 @@ if (isset($_SESSION["nom"])){
 }
 else{
     if(isset($_GET["action"])){
+
         if ($_GET["action"] == "login")
             login($_POST["nom"],$_POST["mdp"]);
+
         if ($_GET["action"] == "register"){
             register($_POST["Nom"],$_POST["Mail"],$_POST["Mdp"]);
             require "vue/vueinscription.php";
         }
             
         if ($_GET["action"] == "inscription"){
+            require "vue/vueinscription.php";
+        }
+
+        if ($_GET["action"] == "connexion"){
             require "vue/vueinscription.php";
         }
         
